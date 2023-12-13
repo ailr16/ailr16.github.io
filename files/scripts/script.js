46e1b90752ea4c5fcfb908e303dc085e0cc27ced
Angel Lozano <angel_ivan_5@outlook.com>
2023-12-12 23:52:19 -0600

    Added carousel for projects section

--- files/scripts/script.js
+++ files/scripts/script.js
@@ -1,36 +1,24 @@
-let slideIndex = [1,1];
-/* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex[no]-1].style.display = "block";
-}
+//Scripts for carousels
 
 const listOfCardElements = document.querySelectorAll('.Carousel_card');
 const cardContainer      = document.querySelector('.Carousel_card_container');
 
-listOfCardElements.forEach((cardElement, index) => {
-  cardElement.addEventListener('click', () => {
-    const scrollLeft = index * listOfCardElements[0].offsetWidth;
-    cardContainer.scrollTo({ left: scrollLeft, behavior: 'smooth' });
-  });
-});
-
 function Carousel_move_slide(direction) {
   const container = document.querySelector('.Carousel_card_container');
+  const scrollAmount = 400; // Adjust this value based on your card width
+
+  if (direction === -1) {
+    container.scrollLeft -= scrollAmount;
+  } else {
+    container.scrollLeft += scrollAmount;
+  }
+}
+
+const listOfProjectsCardElements = document.querySelectorAll('.ProjectsCarousel_card');
+const projectsCardContainer      = document.querySelector('.ProjectsCarousel_card_container');
+
+function ProjectsCarousel_move_slide(direction) {
+  const container = document.querySelector('.ProjectsCarousel_card_container');
   const scrollAmount = 400; // Adjust this value based on your card width
 
   if (direction === -1) {
