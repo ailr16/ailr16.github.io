553c8a4c0e625421f44d8f6b7f42fd034144b54d
Angel Lozano <angel_ivan_5@outlook.com>
2024-07-03 22:57:46 -0600

    Feature added: drop down when title section is clicled

--- files/scripts/script.js
+++ files/scripts/script.js
@@ -1,5 +1,5 @@
 //Scripts for carousels
-
+/*
 const listOfCardElements = document.querySelectorAll('.AboutMe_container_carouselContainer_cardContainer_card');
 const cardContainer      = document.querySelector('.AboutMe_container_carouselContainer_cardContainer');
 
@@ -25,5 +25,43 @@ function ProjectsCarousel_move_slide(direction) {
     container.scrollLeft -= scrollAmount;
   } else {
     container.scrollLeft += scrollAmount;
+  }
+}*/
+
+var AboutMeContent   = document.getElementById( "AboutMe_content" );
+var ProjectsContent  = document.getElementById( "Projects_content" );
+var EducationContent = document.getElementById( "Education_content" );
+
+function toggleAboutMe() { 
+  if(AboutMeContent.style.display === "none" || AboutMeContent.style.display === ""){
+    AboutMeContent.style.display   = "block";
+    ProjectsContent.style.display  = "none";
+    EducationContent.style.display = "none";
+  }
+  else{
+    AboutMeContent.style.display = "none";
+  }
+}
+
+function toggleProjects() {
+  if(ProjectsContent.style.display === "none" || ProjectsContent.style.display === ""){
+    AboutMeContent.style.display   = "none";
+    ProjectsContent.style.display  = "block";
+    EducationContent.style.display = "none";
+  }
+  else{
+    ProjectsContent.style.display = "none";
+  }
+}
+
+function toggleEducation() {
+  
+  if(EducationContent.style.display === "none" || EducationContent.style.display === ""){
+    AboutMeContent.style.display   = "none";
+    ProjectsContent.style.display  = "none";
+    EducationContent.style.display = "block";
+  }
+  else{
+    EducationContent.style.display = "none";
   }
 }
