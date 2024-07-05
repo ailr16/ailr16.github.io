6094f83438c7577be3d5e53db4c704a09db862d3
Angel Lozano <angel_ivan_5@outlook.com>
2024-07-05 00:36:59 -0600

    Auto scroll on click

--- files/scripts/script.js
+++ files/scripts/script.js
@@ -1,67 +1,49 @@
-//Scripts for carousels
-/*
-const listOfCardElements = document.querySelectorAll('.AboutMe_container_carouselContainer_cardContainer_card');
-const cardContainer      = document.querySelector('.AboutMe_container_carouselContainer_cardContainer');
-
-function Carousel_move_slide(direction) {
-  const container = document.querySelector('.AboutMe_container_carouselContainer_cardContainer');
-  const scrollAmount = 400; // Adjust this value based on your card width
-
-  if (direction === -1) {
-    container.scrollLeft -= scrollAmount;
-  } else {
-    container.scrollLeft += scrollAmount;
-  }
-}
-
-const listOfProjectsCardElements = document.querySelectorAll('.Projects_container_carouselContainer_cardContainer_card');
-const projectsCardContainer      = document.querySelector('.Projects_container_carouselContainer_cardContainer');
-
-function ProjectsCarousel_move_slide(direction) {
-  const container = document.querySelector('.Projects_container_carouselContainer_cardContainer');
-  const scrollAmount = 400; // Adjust this value based on your card width
-
-  if (direction === -1) {
-    container.scrollLeft -= scrollAmount;
-  } else {
-    container.scrollLeft += scrollAmount;
-  }
-}*/
-
 var AboutMeContent   = document.getElementById( "AboutMe_content" );
 var ProjectsContent  = document.getElementById( "Projects_content" );
 var EducationContent = document.getElementById( "Education_content" );
+var ProfileContainer = document.getElementById( "Profile_container" );
 
 function toggleAboutMe() { 
+  let elementToView = document.getElementById( "AboutMe_header" );
+
   if(AboutMeContent.style.display === "none" || AboutMeContent.style.display === ""){
     AboutMeContent.style.display   = "block";
     ProjectsContent.style.display  = "none";
     EducationContent.style.display = "none";
+    elementToView.scrollIntoView({behavior: "smooth"});
   }
   else{
     AboutMeContent.style.display = "none";
+    ProfileContainer.scrollIntoView({behavior: "smooth"});
   }
 }
 
 function toggleProjects() {
+  let elementToView = document.getElementById( "Projects_header" );
+
   if(ProjectsContent.style.display === "none" || ProjectsContent.style.display === ""){
     AboutMeContent.style.display   = "none";
     ProjectsContent.style.display  = "block";
     EducationContent.style.display = "none";
+    elementToView.scrollIntoView({behavior: "smooth"});
   }
   else{
     ProjectsContent.style.display = "none";
+    ProfileContainer.scrollIntoView({behavior: "smooth"});
   }
 }
 
 function toggleEducation() {
-  
+  let elementToView = document.getElementById( "Education_header" );
+
   if(EducationContent.style.display === "none" || EducationContent.style.display === ""){
     AboutMeContent.style.display   = "none";
     ProjectsContent.style.display  = "none";
     EducationContent.style.display = "block";
+    elementToView.scrollIntoView({behavior: "smooth"});
   }
   else{
     EducationContent.style.display = "none";
+    ProfileContainer.scrollIntoView({behavior: "smooth"});
   }
 }
